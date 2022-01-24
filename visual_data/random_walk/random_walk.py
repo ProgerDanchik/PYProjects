600dd925a4073848d4cc77a14b5675bbac6f2715
styleprog <89692175+ProgerDanchik@users.noreply.github.com>
2022-01-24 12:34:01 +0300

    Класс для генерации объектов с случайным шагом
    Класс для генерации объектов с случайным шагом для визуализации графов

---
+++ visual_data/random_walk/random_walk.py
@@ -0,0 +1,40 @@
+from random import choice
+
+
+class RandomWalk:
+    """Класс для генерирования случайных блужданий"""
+
+    def __init__(self, num_points=5000):
+        """Инициализирует атрибуты блуждания"""
+        self.num_points = num_points
+
+        # Все блуждения начинаются с точки (0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        """Вычисляет все точки блуждания"""
+
+        # Шаги генерируются до достижения нужной длины
+        while len(self.x_values) < self.num_points:
+            # Определение направления и длины перемещения
+            x_step = get_step()
+            y_step = get_step()
+
+            # Отклонение нулевых перемещений
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Вычисления следующих значений x и y
+            next_x = self.x_values[-1] + x_step
+            next_y = self.y_values[-1] + y_step
+
+            self.x_values.append(next_x)
+            self.y_values.append(next_y)
+
+
+def get_step():
+    direction = choice([1, -1])
+    distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    step = direction * distance
+    return step
